34fb41f1633a5329d0045d5444c2c6e72e50ac75
obeka <46748217+obeka@users.noreply.github.com>
2020-11-06 15:43:27 +0100

    auth page done

--- backend/app.js
+++ backend/app.js
@@ -1,61 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const jwt = require("jsonwebtoken");
+
 const cors = require("cors");
-const app = express();
-app.use(bodyParser.json());
+const userRoutes = require("./routes/user-routes")
+
+//Models
 const User = require("./models/user-model");
 
+const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+
+//Route Middleware
+app.use("/user", userRoutes)
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.post("/signup", async (req, res, next) => {
-  const { username, email, password } = req.body;
 
-  let existingUser;
-  try {
-    existingUser = await User.findOne({ email: email });
-  } catch (err) {
-    console.log("Signup failed.");
-  }
-  if (existingUser) {
-
-  }
-  const createdUser = new User({
-    username,
-    email,
-    password,
-    todos: [],
-  });
-
-  try {
-    await createdUser.save();
-  } catch (err) {
-    console.log("Signup failed");
-    console.log(err + "asdasdasd");
-  }
-
-  let token;
-  try {
-    token = jwt.sign(
-      { userId: createdUser.id, email: createdUser.email },
-      "todosaresecret",
-      { expiresIn: "1h" }
-    );
-  } catch (err) {
-    console.log("Signing up failed, please try again later.");
-    console.log(err);
-  }
-  res.status(201).json({
-    userId: createdUser.id,
-    email: createdUser.email,
-    token: token,
-  });
-});
 
 mongoose
   .connect(
